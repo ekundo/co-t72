16f0032badcf64463d5525e1396d0520f6677334
Yauheni Kundo <e.kundo@gmail.com>
2026-09-15 23:33:08 +0300

    fix: HDIR не попадал на квазидиск выпуска
    patch-co.sh собирает образ только с CO и его комплектом, а HDIR в прошлых
    выпусках дописывался на квазидиск руками. Скрипт выпуска этого не делал, и в
    v2.2.0 образы уехали без него -- хотя и README, и описание выпуска обещают, что
    он там есть. Кладём обе программы в образ рядом с CO.

--- make-release.sh
+++ make-release.sh
@@ -56,6 +56,10 @@ for v in f h hx k; do
     cp "$WORK/$v/co-t72.fdd" "$kit/$name.fdd"
     sh "$HERE/tools/relnote.sh" "$v" "$VERSION" > "$kit/README.txt"
 
+    # HDIR кладём и на квазидиск: patch-co.sh собирает образ только с CO.
+    python3 "$HERE/tools/kdimg.py" put "$WORK/$v/co-t72.edd" "$WORK/HDIR.COM"   >/dev/null
+    python3 "$HERE/tools/kdimg.py" put "$WORK/$v/co-t72.edd" "$WORK/HDIREN.COM" >/dev/null
+
     ( cd "$WORK/kit-$v" && rm -f "$OUT/$name.zip" && zip -qr "$OUT/$name.zip" "$name" )
     cp "$WORK/$v/co-t72.edd" "$OUT/$name.edd"
     echo "  $name.zip: $(stat -f%z "$OUT/$name.zip") байт, $name.edd: $(stat -f%z "$OUT/$name.edd") байт"
